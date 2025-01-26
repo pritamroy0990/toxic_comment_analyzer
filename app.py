a5c9fabd8a454b1e558cce7f850f6b1b8daae562
pritamroy0990 <pritamroy0990@gmail.com>
2025-01-26 17:03:47 +0600

    add file

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
-# @st.(allow_output_mutation=True)
 @st.cache_resource
 def get_model():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -25,7 +24,6 @@ d = {
 
 if user_input and button :
     test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
-    # test_sample
     output = model(**test_sample)
     # st.write("Logits: ",output.logits)
     y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
