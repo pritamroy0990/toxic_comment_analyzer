b587305ae03110eea93b9e2f6bab01ca496be0ac
pritamroy0990 <pritamroy0990@gmail.com>
2025-01-26 16:47:17 +0600

    update

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import numpy as np
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
-@st.cache(allow_output_mutation=True)
+# @st.(allow_output_mutation=True)
+@st.cache_resource
 def get_model():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
+    model = BertForSequenceClassification.from_pretrained("pritam0990/toxic_analizer")
     return tokenizer,model
 
 
 tokenizer,model = get_model()
 
+st.header('Comment Analyze')
 user_input = st.text_area('Enter Text to Analyze')
 button = st.button("Analyze")
 
@@ -25,6 +27,6 @@ if user_input and button :
     test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
     # test_sample
     output = model(**test_sample)
-    st.write("Logits: ",output.logits)
+    # st.write("Logits: ",output.logits)
     y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
     st.write("Prediction: ",d[y_pred[0]])
